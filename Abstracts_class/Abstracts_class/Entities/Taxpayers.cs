8af8c3bb55d425ff0e4daed9853a05de43e6aa15
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-08 17:02:18 -0300

    Created the project for resolution of the exercise. Start of the implementation of abstract class contributors

---
+++ Abstracts_class/Abstracts_class/Entities/Taxpayers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts_class.Entities
+{
+    abstract class TaxPayers
+    {
+        public string Name { get; set; }
+        public double IncomeAnual { get; set; }
+
+        public TaxPayers(string name, double incomeAnual)
+        {
+            Name = name;
+            IncomeAnual = incomeAnual;
+        }
+
+        public abstract double TaxesPaid();
+
+    }
+}
