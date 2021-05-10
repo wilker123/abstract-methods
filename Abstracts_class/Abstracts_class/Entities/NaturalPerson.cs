566572bcd3ec7b798a42d7b2c168eb39abc0d03a
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-09 23:58:51 -0300

    Method for calculating the tax of the complete natural person

--- Abstracts_class/Abstracts_class/Entities/NaturalPerson.cs
+++ Abstracts_class/Abstracts_class/Entities/NaturalPerson.cs
@@ -15,5 +15,20 @@ namespace Abstracts_class.Entities
         {
             HealthExpenditures = healthExpenditures;
         }
+
+        public override double TaxesPaid()
+        {
+            double tax = 0.0;
+            if (IncomeAnual < 20000.00)
+            {
+                tax = (IncomeAnual * 0.15) - (HealthExpenditures * 0.5);
+            }
+            else
+            {
+                tax = (IncomeAnual * 0.25) - (HealthExpenditures * 0.5);
+            }
+            return tax;
+        }
+
     }
 }
