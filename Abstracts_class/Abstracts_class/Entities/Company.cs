252d750083755c521fe5252bdbade0350a8076f4
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-09 23:18:01 -0300

    created the individual entities and company with their builders and both inheriting from abstract class contributors.

---
+++ Abstracts_class/Abstracts_class/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts_class.Entities
+{
+    class Company : TaxPayers
+    {
+        public int NumberEmployees { get; set; }
+
+        public Company(int numberEmployees, string name, double incomeAnual)
+            : base(name, incomeAnual)
+        {
+            NumberEmployees = numberEmployees;
+        }
+    }
+}
---
+++ Abstracts_class/Abstracts_class/Entities/NaturalPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts_class.Entities
+{
+    class NaturalPerson : TaxPayers
+    {
+        public double HealthExpenditures { get; set; }
+
+        public NaturalPerson(double healthExpenditures, string name, double incomeAnual)
+            : base(name, incomeAnual)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+    }
+}
